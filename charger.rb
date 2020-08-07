4885bed92e0b1cf8ca4161de1ae6ba5fa83eac6b
Jeff Kowalski <jeff.kowalski@gmail.com>
2020-08-06 21:27:04 -0700

    allow per-car charge limit; required change in yaml format

--- charger.rb
+++ charger.rb
@@ -10,30 +10,31 @@ CREDENTIALS_PATH = File.join(Dir.home, '.credentials', 'charger.yaml')
 credentials = YAML.load_file CREDENTIALS_PATH
 
 Mail.defaults do
-  delivery_method :smtp, credentials['mail_delivery_defaults']
+  delivery_method :smtp, credentials[:mail_delivery_defaults]
 end
 
 influxdb = InfluxDB::Client.new 'tesla'
 state = {}
 range = {}
 
-credentials['cars'].keys.each do |name|
+credentials[:cars].keys.each do |name|
   result = influxdb.query "select last(value) from charging_state where display_name='#{name}'"
   state[name] = result[0]['values'][0]['last']
   result = influxdb.query "select last(value) from est_battery_range where display_name='#{name}'"
   range[name] = result[0]['values'][0]['last'].to_f
 end
-credentials['cars'].each do |name, emails|
-  next unless range[name] < 100 && state[name] == 'Disconnected'
 
-  (credentials['cars'].keys - [name]).each do |other|
+credentials[:cars].each do |name, prefs|
+  next unless range[name] < prefs[:limit] && state[name] == 'Disconnected'
+
+  (credentials[:cars].keys - [name]).each do |other|
     next unless state[other] == 'Disconnected' || state[other] == 'Complete' ||
-                (state[other] == 'Stopped' && range[other] > 100)
+                (state[other] == 'Stopped' && credentials[:cars][other][:limit] > 100)
 
-    emails.each do |email|
+    prefs[:notify].each do |email|
       Mail.deliver do
         to email
-        from credentials['sender']
+        from credentials[:sender]
         subject 'Please plug in your car'
         body "Your car has only #{range[name]} miles of range.  Please plug it in to charge."
       end
